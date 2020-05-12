f89f80fa7954737f256156e6166f9063b08ef9d6
murilloreva7x <murillo.ramos@sou.unifeob.edu.br>
2020-05-12 17:47:33 -0300

    modificações iniciais

---
+++ pet_encontrado/lib/carousel.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+final List<String> imgList = [
+  'https://cdn.pixabay.com/photo/2014/03/14/20/13/dog-287420_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2015/11/17/13/13/dogue-de-bordeaux-1047521_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_960_720.jpg',
+];
+
+
+class CarouselWithIndicatorDemo extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CarouselWithIndicatorState();
+  }
+}
+
+class _CarouselWithIndicatorState extends State<CarouselWithIndicatorDemo> {
+  int _current = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+          children: [
+            CarouselSlider(
+              items: imageSliders,
+              options: CarouselOptions(
+                  autoPlay: true,
+                  enlargeCenterPage: false,
+                  aspectRatio: 1.3,
+
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _current = index;
+                    });
+                  }
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: imgList.map((url) {
+                int index = imgList.indexOf(url);
+                return Container(
+                  width: 8.0,
+                  height: 8.0,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _current == index
+                        ? Color.fromRGBO(0, 0, 0, 0.9)
+                        : Color.fromRGBO(0, 0, 0, 0.4),
+                  ),
+                );
+              }).toList(),
+            ),
+          ]
+      );
+  }
+}
+
+final List<Widget> imageSliders = imgList.map((item) => Container(
+  child: Container(
+    margin: EdgeInsets.all(5.0),
+    child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        child: Stack(
+          children: <Widget>[
+            Image.network(item,  fit: BoxFit.cover, width: 1000),
+            Positioned(
+              bottom: 0.0,
+              left: 0.0,
+              right: 0.0,
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromARGB(200, 0, 0, 0),
+                      Color.fromARGB(0, 0, 0, 0)
+                    ],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+              ),
+            ),
+          ],
+        )
+    ),
+  ),
+)).toList();
+
--- pet_encontrado/lib/main.dart
+++ pet_encontrado/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'carousel.dart';
+
 
 void main() {
   runApp(MaterialApp(
@@ -18,7 +20,7 @@ class _HomeState extends State<Home> {
         appBar: AppBar(
           title: Text("Pet encontrado"),
           centerTitle: true,
-          backgroundColor: Colors.lightBlue,
+          backgroundColor: Colors.blueAccent,
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.keyboard_backspace),
@@ -28,48 +30,70 @@ class _HomeState extends State<Home> {
         ),
         backgroundColor: Colors.white,
         body: SingleChildScrollView(
-          padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+          padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Icon(
-                Icons.image,
-                size: 300.0,
-                color: Colors.lightBlue,
-              ),
-              TextField(
-                keyboardType: TextInputType.multiline,
-                decoration: InputDecoration(
-                    labelText: "Qual dia o Pet foi encontrado?",
-                    labelStyle: TextStyle(color: Colors.lightBlue)),
-                textAlign: TextAlign.left,
-                style: TextStyle(color: Colors.lightBlue, fontSize: 20.0),
-              ),
-              TextField(
-                keyboardType: TextInputType.multiline,
-                decoration: InputDecoration(
-                    labelText: "Qual endereco o Animal foi encontrado?",
-                    labelStyle: TextStyle(color: Colors.lightBlue)),
-                textAlign: TextAlign.left,
-                style: TextStyle(color: Colors.lightBlue, fontSize: 20.0),
-              ),
-              ButtonBar(
-                children: <Widget>[
-                  FlatButton(
-                    child: Text(
-                      "Cancelar",
-                      style: TextStyle(color: Colors.lightBlue, fontSize: 25.0),
-                    ),
-                    onPressed: () {},
+              CarouselWithIndicatorDemo(),
+              Padding (
+                padding: const EdgeInsets.all(8.0),
+                child:
+                Align(
+                  child:
+                  Text ("Pet encontrado 10/05 as 18:00",
+                    style: TextStyle(color: Colors.black45, fontSize: 20.0, fontWeight: FontWeight.bold),
                   ),
-                  FlatButton(
-                    child: Text(
-                      "Mapa",
+                  alignment: Alignment.center,
+                ),
+              ),
+              Padding (
+                padding: const EdgeInsets.all(8.0),
+                child:
+                Align(
+                  child:
+                    Text ("Quem achou: Anderson Luis Ribeiro",
+                      style: TextStyle(color: Colors.black45, fontSize: 20.0, fontWeight: FontWeight.bold),
+                    ),
+                  alignment: Alignment.center,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 80.0),
+                child:
+                Align(
+                  child:
+                  Text ("Este Pet é Seu?",
+                    style: TextStyle(color: Colors.black45, fontSize: 25.0, fontWeight: FontWeight.bold),
+                  ),
+                  alignment: Alignment.center,
+                ),
+              ),
+
+          ButtonBar(
+                children: <Widget>[
+                  FlatButton.icon(
+                    icon: Icon(
+                      Icons.arrow_back
+                    ),
+                    label: Text('Não',
                       style: TextStyle(color: Colors.white, fontSize: 25.0),
                     ),
-                    color: Colors.lightBlue,
+
+                    color: Colors.blueAccent,
                     onPressed: () {},
-                  )
+
+                  ),
+                  FlatButton.icon(
+                    icon: Icon(
+                      Icons.pin_drop
+                    ),
+                    label: Text('Sim',
+                      style: TextStyle(color: Colors.white, fontSize: 25.0),
+                    ),
+
+                    color: Colors.blueAccent,
+                    onPressed: () {},
+                  ),
                 ],
                 alignment: MainAxisAlignment.center,
                 mainAxisSize: MainAxisSize.max,
