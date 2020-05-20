09b7252e91b5e2095d59c14474b0e059f75be39d
murilloreva7x <murillo.ramos@sou.unifeob.edu.br>
2020-05-19 22:16:51 -0300

    botoes

--- pet_encontrado/lib/petEncontrado.dart
+++ pet_encontrado/lib/petEncontrado.dart
@@ -17,138 +17,166 @@ class _HomeState extends State<Home> {
         body: SingleChildScrollView(
           padding: EdgeInsets.fromLTRB(
               8.0, 25.0, 8.0, 10.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              CarouselWithIndicatorDemo(),
-              Padding (
-                padding: const EdgeInsets.all(10.0
-                ),
-                child:
-                Align(
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                CarouselWithIndicatorDemo(),
+                Padding (
+                  padding: const EdgeInsets.all(10.0
+                  ),
                   child:
-                  Text (
-                    "Pet encontrado 10/05 as 18:00",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                      fontFamily: 'Roboto',
-                      letterSpacing: 0.5,
+                  Align(
+                    child:
+                    Row(
+                      children: <Widget>[
+                        Text (
+                          "Pet encontrado:  ",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 20.0,
+                            fontFamily: 'Roboto',
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                        Text (
+                          "dia:  ",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16.0,
+                            fontFamily: 'Roboto',
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                        Text (
+                          "Hora: ",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16.0,
+                            fontFamily: 'Roboto',
+                            letterSpacing: 0.5,
+                          ),
+                        ),
+                      ],
                     ),
+                    alignment: Alignment.bottomLeft,
                   ),
-                  alignment: Alignment.bottomLeft,
                 ),
-              ),
-              Padding (
-                padding: const EdgeInsets.all(10.0),
-                child:
-                Align(
+                Padding (
+                  padding: const EdgeInsets.all(10.0),
                   child:
-                  Text (
-                    "Quem achou: Anderson Luis Ribeiro",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold,
-                      fontFamily: 'Roboto',
-                      letterSpacing: 0.5,
-                    ),
-                  ),
-
-                  alignment: Alignment.bottomLeft,
-                ),
-              ),
-              Padding (
-                padding: const EdgeInsets.all(20.0),
-                child:
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: const <Widget>[
-                      Icon(
-                        MdiIcons.whatsapp,
-                        color: Colors.green,
-                        size: 50.0,
-                      ),
-                      Icon(
-                        Icons.call,
-                        color: Colors.blueAccent,
-                        size: 50.0,
-                      ),
-                      Icon(
-                        Icons.email,
-                        color: Colors.red,
-                        size: 50.0,
-                      )
-                    ],
-                  ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    top: 2.0
-                ),
-                child:
-                Align(
-                  child:
-                  Text ("Este Pet é Seu?",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                      fontFamily: 'Roboto',
-                      letterSpacing: 0.0,
-                    ),
-                  ),
-                  alignment: Alignment.center,
-                ),
-              ),
-
-              ButtonBar(
-                children: <Widget>[
-                  FlatButton.icon(
-                    icon: Icon(
-                        Icons.arrow_back_ios
-                    ),
-                    label: Text('Não',
+                  Align(
+                    child:
+                    Text (
+                      "Quem achou: ",
                       style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14.0,
+                          color: Colors.black,
+                          fontSize: 18.0,
                         fontFamily: 'Roboto',
-                        letterSpacing: 1.0,
+                        letterSpacing: 0.5,
                       ),
                     ),
 
-                    color: Colors.blueAccent,
-                    onPressed: () {},
-                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0))
-
+                    alignment: Alignment.bottomLeft,
                   ),
-                  FlatButton.icon(
-                    icon: Icon(
-                        Icons.location_on
+                ),
+                Padding (
+                  padding: const EdgeInsets.all(20.0),
+                  child:
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const <Widget>[
+                        Icon(
+                          MdiIcons.whatsapp,
+                          color: Colors.black54,
+                          size: 50.0,
+                        ),
+                        Icon(
+                          Icons.call,
+                          color: Colors.black54,
+                          size: 50.0,
+                        ),
+                        Icon(
+                          Icons.email,
+                          color: Colors.black54,
+                          size: 50.0,
+                        )
+                      ],
                     ),
-                    label: Text('Sim',
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      top: 2.0
+                  ),
+                  child:
+                  Align(
+                    child:
+                    Text ("ESTE PET É SEU?",
                       style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14.0,
+                          color: Colors.black,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
                         fontFamily: 'Roboto',
-                        letterSpacing: 1.0,
+                        letterSpacing: 0.6,
                       ),
                     ),
-
-                      color: Colors.blueAccent,
-                    onPressed: () {},
-                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0))
+                    alignment: Alignment.center,
                   ),
-                ],
-                buttonHeight: 36.0,
-                buttonMinWidth: 113.0,
-                buttonPadding: EdgeInsets.all(5.0),
+                ),
 
-                alignment: MainAxisAlignment.spaceBetween,
-                mainAxisSize: MainAxisSize.max,
-              )
-            ],
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    ButtonBar(
+                      children: <Widget>[
+                        FlatButton.icon(
+                          icon: Icon(
+                              Icons.arrow_back_ios
+                          ),
+                          label: Text('NÃO',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18.0,
+                              fontFamily: 'Roboto',
+                              letterSpacing: 1.0,
+                            ),
+                          ),
+
+                          color: Colors.blueAccent,
+                          onPressed: () {},
+                            shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0))
+
+                        ),
+                        FlatButton.icon(
+                          icon: Icon(
+                              Icons.location_on
+                          ),
+                          label: Text('SIM',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                              fontFamily: 'Roboto',
+                              letterSpacing: 1.0,
+                            ),
+                          ),
+
+                            color: Colors.blueAccent,
+                          onPressed: () {},
+                            shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0))
+                        ),
+                      ],
+                      buttonHeight: 42.0,
+                      buttonMinWidth: 113.0,
+                      //buttonPadding: EdgeInsets.all(5.0),
+                      buttonPadding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 1.0),
+                      alignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.max,
+                    ),
+                  ],
+                )
+              ],
+            ),
           ),
         ));
   }
